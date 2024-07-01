ea2b15c0cc41aac438ddb7973354c2c2b31999ee
Nv7-Github <junk4Nv7@gmail.com>
2024-07-01 13:17:09 -0700

    Initial commit

---
+++ Core/Inc/peripheral.h
@@ -0,0 +1,15 @@
+/*
+ * peripheral.h
+ *
+ *  Created on: Jul 1, 2024
+ *      Author: nv
+ */
+
+#ifndef INC_PERIPHERAL_H_
+#define INC_PERIPHERAL_H_
+
+#include "main.h"
+
+
+
+#endif /* INC_PERIPHERAL_H_ */
---
+++ Core/Src/peripheral.c
@@ -0,0 +1,12 @@
+/*
+ * peripheral.c
+ *
+ *  Created on: Jul 1, 2024
+ *      Author: nv
+ */
+
+
+#include "peripheral.c"
+
+
+
